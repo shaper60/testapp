ee60255a821845cf61794b7d9aefa6bf488a6e32
Hiroyuki Ogi <shaper60@gmail.com>
2015-01-13 18:13:55 +0900

    追加

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+app  = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "Hello World!"
+	return "Helloooo!"
 
 if __name__ == '__main__':
 	app.run()
